0eb2e4a6852422c3a2cb45ee6df841c0bb703c20
Jerzy Jamroz <jerzy.jamroz@gmail.com>
2020-02-10 13:20:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Supercycle Application for ESS
+--
+
+# Building
+* sudo yum install $(cat requirements.txt)
+* make
+
---
+++ iocBoot/iocsupercycleEngine/st.cmd
@@ -0,0 +1,21 @@
+#!../../bin/linux-x86_64/supercycleEngine
+
+#- You may have to change supercycleEngine to something else
+#- everywhere it appears in this file
+
+< envPaths
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/supercycleEngine.dbd"
+supercycleEngine_registerRecordDeviceDriver pdbbase
+
+## Load record instances
+#dbLoadRecords("db/xxx.db","user=jerzy")
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=jerzy"
