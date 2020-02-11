9ddc9156122d9bc48b856c3f63d360a187c87661
Jerzy Jamroz <jerzy.jamroz@esss.se>
2020-02-11 12:44:45 +0100

    Adaptation tests.

--- iocBoot/iocsupercycleEngine/st.cmd
+++ iocBoot/iocsupercycleEngine/st.cmd
@@ -13,6 +13,7 @@ supercycleEngine_registerRecordDeviceDriver pdbbase
 
 ## Load record instances
 #dbLoadRecords("db/xxx.db","user=jerzy")
+dbLoadRecords("db/testProc.db")
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
