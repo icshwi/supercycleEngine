6f581f4ac7f0a6742a376f3f259fa1a9c32bcdca
Jerzy Jamroz <jerzy.jamroz@esss.se>
2020-02-14 10:42:41 +0100

    wf send.

--- supercycleEngineApp/src/dbufProc.c
+++ supercycleEngineApp/src/dbufProc.c
@@ -47,12 +47,46 @@ static long init_dbuf_wf(int after)
     print(1, 1111);
 }
 
-static long init_dbuf_record_wf(struct waveformRecord *pwave)
+static long init_dbuf_record_wf(struct waveformRecord *prec)
 {
     print(2, 2222);
 }
 
-static long read_dbuf_wf(struct waveformRecord *pwave)
+static long read_dbuf_wf(struct waveformRecord *prec)
 {
     print(3, 3333);
+    long nRequest = prec->nelm;
+
+    int inta[3000] = {5, 6, 7, 8};
+
+    dbPutLink(&prec->inp, prec->ftvl, inta, 4);
+
+    /*
+    if (!prec->rarm)
+    {
+        // If not "armed" then put
+        prec->rarm = 0;
+        db_post_events(prec, &prec->rarm, DBE_VALUE | DBE_ARCHIVE);
+        dbPutLink(&prec->inp, prec->ftvl, prec->bptr, prec->nord);
+    }
+    else
+    {
+        // If "armed" then get
+        if (prec->inp.type != CONSTANT)
+            (void)dbGetLink(&prec->inp, prec->ftvl, prec->bptr, 0, &nRequest);
+        if (nRequest > 0)
+        {
+            prec->nord = nRequest;
+            if (prec->tsel.type == CONSTANT &&
+                prec->tse == epicsTimeEventDeviceTime)
+                dbGetTimeStamp(&prec->inp, &prec->time);
+        }
+    }
+
+    int *tmp = (int *)prec->bptr;
+
+    print(tmp[0], 10);
+    */
+
+    return 0;
 }
