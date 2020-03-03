7df64ce00ac03df7fd366f736d6c607c635e9d66
Jerzy Jamroz <jerzy.jamroz@esss.se>
2020-03-03 14:55:27 +0100

    Update.

--- cmd/_TD-M-EVG.cmd
+++ cmd/_TD-M-EVG.cmd
@@ -3,13 +3,13 @@
 epicsEnvSet("TOP", "$(E3_CMD_TOP)/..")
 iocshLoad("$(TOP)/iocsh/tsi.iocsh")
 
-epicsEnvSet("AS", "MTCA5U")
-epicsEnvSet("DS", "EVG")
-epicsEnvSet("PEVG", "$(AS):$(DS)-01:")
+epicsEnvSet("IOC", "MTCA5U")
+epicsEnvSet("DEV", "EVG")
+epicsEnvSet("PEVG", "$(IOC)-$(DEV):")
 
 require mrfioc2, 2.2.0-rc7
 
-iocshLoad("$(TOP)/iocsh/evmi.iocsh", "AS=$(AS), DS=$(DS), PCIID=$(MTCA_5U_PCIID7)")
+iocshLoad("$(TOP)/iocsh/evmi.iocsh", "IOC=$(IOC), DEV=$(DEV), PCIID=$(MTCA_5U_PCIID7)")
 
 iocInit()
 
