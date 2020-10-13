dc2fa92ed5da65a71cea180d925854fee9041b39
Jerzy Jamroz <jerzy.jamroz@esss.se>
2020-10-13 14:45:51 +0200

    devel: yaml handler

---
+++ supercycleEngineApp/src/yml.cpp
@@ -0,0 +1,19 @@
+/**
+ * @SPDX-License-Identifier LGPL-2.1-only
+ * @author Jerzy Jamroz (jerzy.jamroz@ess.eu)
+ * @date 2020-10-12
+ */
+
+#include "yml.hpp"
+#include "dlog.hpp"
+#include "cmnbase.hpp"
+
+#include <yaml-cpp/yaml.h>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+namespace io
+{
+
+} // namespace io
---
+++ supercycleEngineApp/src/yml.hpp
@@ -0,0 +1,25 @@
+/**
+ * @SPDX-License-Identifier LGPL-2.1-only
+ * @author Jerzy Jamroz (jerzy.jamroz@ess.eu)
+ * @date 2020-10-12
+ */
+
+#ifndef YML_HPP_
+#define YML_HPP_
+
+#include "scenv.hpp"
+
+#include <epicsTypes.h>
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <yaml-cpp/yaml.h>
+#include <map>
+
+namespace io
+{
+
+} // namespace io
+
+#endif // YML_HPP_
