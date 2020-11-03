f3d226f234246126bcf0f4204183e870c7b3e691
Jerzy Jamroz <jerzy.jamroz@esss.se>
2020-11-03 11:22:58 +0100

    feat: databuffer exposed as feedback pvs

--- supercycleEngineApp/src/devEngine.cpp
+++ supercycleEngineApp/src/devEngine.cpp
@@ -19,36 +19,36 @@
 
 static long initEngine()
 {
-    iocVars2IO();
-    return 0;
+  iocVars2IO();
+  return 0;
 }
 
 static long ioEngine(aSubRecord *prec)
 {
-    static io::IOBlock &io_block = io::RegisteredIOBlock();
-    // Configure new cycle
-    io_block.dbSync(RegisteredStrOutMap);
-    // Change the table if requested
-    sctableSwitch(io_block);
-    // Engine cycle
-    engineCycle(io_block);
-    // Update the meta
-    epicsUInt64 *pvalaU64 = (epicsUInt64 *)prec->vala;
-    epicsUInt32 *pvalaU32 = (epicsUInt32 *)prec->vala;
-    pvalaU64[0] = io_block.cId; // 0,1
-    pvalaU32[2] = (epicsUInt32)io_block.cPeriod;
-    // Update the Dbuf - neva , novb (max)
-    prec->nevb = cmn::vec2p<epicsUInt32>(prec->valb, io_block.dbuf.vallist());
-    prec->nevc = cmn::vec2p<epicsUInt32>(prec->valc, io_block.Seq.getSeqTst());
-    prec->nevd = cmn::vec2p<epicsUInt32>(prec->vald, io_block.Seq.getSeqEvt());
-    prec->neve = cmn::vec2p<epicsUInt32>(prec->vale, io_block.Seq.getSeqVec());
+  static io::IOBlock &io_block = io::RegisteredIOBlock();
+  // Configure new cycle
+  io_block.dbSync(RegisteredStrOutMap);
+  // Change the table if requested
+  sctableSwitch(io_block);
+  // Engine cycle
+  engineCycle(io_block);
+  // Update the meta
+  epicsUInt64 *pvalaU64 = (epicsUInt64 *)prec->vala;
+  epicsUInt32 *pvalaU32 = (epicsUInt32 *)prec->vala;
+  pvalaU64[0] = (epicsUInt64)io_block.cId; // 0,1
+  pvalaU32[2] = (epicsUInt32)io_block.cPeriod;
+  // Update the Dbuf - neva , novb (max)
+  prec->nevb = cmn::vec2p<epicsUInt32>(prec->valb, io_block.dbuf.vallist());
+  prec->nevc = cmn::vec2p<epicsUInt32>(prec->valc, io_block.Seq.getSeqTst());
+  prec->nevd = cmn::vec2p<epicsUInt32>(prec->vald, io_block.Seq.getSeqEvt());
+  prec->neve = cmn::vec2p<epicsUInt32>(prec->vale, io_block.Seq.getSeqVec());
 
-    return 0;
+  return 0;
 }
 
 // Register the function
 extern "C"
 {
-    epicsRegisterFunction(initEngine);
-    epicsRegisterFunction(ioEngine);
+  epicsRegisterFunction(initEngine);
+  epicsRegisterFunction(ioEngine);
 }
--- supercycleEngineApp/src/iocVars.cpp
+++ supercycleEngineApp/src/iocVars.cpp
@@ -22,14 +22,15 @@ epicsExportAddress(int, PscUs);
 
 void iocVars2IO()
 {
-    static io::LOGCONFIG &LOGCFG = io::RegisteredLOGCFG();
-    static io::IOBlock &io_block = io::RegisteredIOBlock();
+  static io::LOGCONFIG &LOGCFG = io::RegisteredLOGCFG();
+  static io::IOBlock &io_block = io::RegisteredIOBlock();
 
-    static io::typelog *const piodebug = (io::typelog *)&iodebug;
-    LOGCFG.init(true, piodebug);
+  static io::typelog *const piodebug = (io::typelog *)&iodebug;
+  LOGCFG.init(true, piodebug);
 
-    io_block.cId = (epicsUInt64)round((cmn::epicssTstSysNowSec() - EPICS2020s) / PscUs * 1000000);
-    io_block.init(RegisteredCmdMapStrOut);
+  io_block.cId = (epicsUInt64)round((cmn::epicssTstSysNowSec() - EPICS2020s) / PscUs * 1000000);
+  //io_block.cId = 1099511627776;
+  io_block.init(RegisteredCmdMapStrOut);
 
-    io::LOG(io::INFO) << "iocVars2IO() iodebug " << iodebug << " PscUs " << PscUs;
+  io::LOG(io::INFO) << "iocVars2IO() iodebug " << iodebug << " PscUs " << PscUs;
 }
