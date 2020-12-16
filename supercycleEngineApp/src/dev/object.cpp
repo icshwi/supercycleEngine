e5c282bf694b343a58de855ea3586846e6dbfed8
Jerzy Jamroz <jerzy.jamroz@gmail.com>
2020-12-16 15:23:17 +0100

    feat: SwBuild added

--- supercycleEngineApp/src/dev/object.cpp
+++ supercycleEngineApp/src/dev/object.cpp
@@ -10,7 +10,7 @@
 namespace SCE
 {
 
-static std::string getSwVersion()
+static std::string getSwVer()
 {
   return SCE_VERSION;
 }
@@ -20,14 +20,25 @@ static std::string getScTable()
   return SCEMEMREG.CSVHandler.getFileName();
 }
 
+static std::string getSwBuild()
+{
+  std::string tmp_ = __DATE__;
+  tmp_ += " ";
+  tmp_ += __TIME__;
+  return tmp_;
+}
+
 } // namespace SCE
+
+#define ObjPropRecIn(obj, prop) _ObjPropFunc[#obj][#prop] = obj::get##prop;
 namespace dev
 {
 
 ObjBase::ObjBase()
 {
-  _ObjPropFunc["SCE"]["SwVer"] = SCE::getSwVersion;
-  _ObjPropFunc["SCE"]["ScTable"] = SCE::getScTable;
+  ObjPropRecIn(SCE, SwVer);
+  ObjPropRecIn(SCE, ScTable);
+  ObjPropRecIn(SCE, SwBuild);
 }
 
 ObjReg& ObjReg::instance()
